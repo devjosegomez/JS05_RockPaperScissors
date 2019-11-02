738286cf284787ac8577575c7c621833eb7c13d9
devjosegomez <devjosegomez@gmail.com>
2019-11-01 22:18:35 -0600

    Added score system that displays Player, Computer and draw points

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const score = document.querySelector(".score");
 const message = document.querySelector(".message");
+const message2 = document.querySelector(".message2");
 const buttons = document.querySelectorAll("button");
+let finalScore = [0,0,0]; //Index 0 Player's score, Index 1 Computer's score, Index 2 Draw
 
 for(let i=0; i<buttons.length; i++){
     buttons[i].addEventListener("click", playGame);
@@ -19,12 +21,28 @@ function playGame(e){
     console.log(playerChoise, computerChoise);
     let winner = checkWinner(playerChoise, computerChoise);
     console.log(winner);
+    if(winner === "Player"){
+        winner += " wins!";
+        finalScore[0]++;
+        message.innerHTML = `<h2>${winner}</h2>`;
+    }else if(winner === "Computer"){
+        winner += " wins!";
+        finalScore[1]++;
+        message.innerHTML = `<h2>${winner}</h2>`;
+    }else if(winner === "Draw"){
+        finalScore[2]++;
+        message.innerHTML = `<h2>${winner}</h2>`;
+    }
+
+    score.innerHTML = `<h1>Player: ${finalScore[0]} vs Computer: ${finalScore[1]} [Draws: ${finalScore[2]}]</h1>`;
+    
+    message2.innerHTML = `<h4>You: (${playerChoise}) vs PC: (${computerChoise})</h4>`;
 }
 
 function checkWinner(plCh, coCh){
     //Draw
     if(plCh === coCh){
-       return "Draw - tie";
+       return "Draw";
     }
     
     //Rock vs Paper
